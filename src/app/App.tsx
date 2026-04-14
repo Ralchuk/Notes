1a16f41dff00d80339b31dbab74a068910af6950
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-14 12:05:55 +0300

    create lab

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import Note from "@/entities/note/note";
+import KeyMismatch from "@/lab/KeyMismatch";
 
 
 function App() {
   
   return (
     <>
-    <Note/>
+    <KeyMismatch/>
     </>
   );
 }
--- src/lab/KeyMismatch.tsx
+++ src/lab/KeyMismatch.tsx
@@ -24,11 +24,13 @@ export default function KeyMismatch(){
             {state.map((item, index) => (
                 <div key={index}>
                     <p>{item.id} - {item.createdAt}</p>
-                    <input defaultValue={item.id}/>   
+                    <input className="px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none focus:border-[#1976d3] font-[Roboto, sans-serif]"
+                     defaultValue={item.id}/>   
                 </div>
             )
             )}
-            <button onClick={handleAddToTop}>Add to top
+            <button  className='flex uppercase px-6 py-2 text-white bg-[#1976d3] rounded-[5px] font-[Roboto, sans-serif] font-medium transition-all duration-200
+    hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer' onClick={handleAddToTop}>Add to top
             </button>
         </div>
     )
