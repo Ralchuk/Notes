2b59616e94667a6689cce3227ba462e7883f779c
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-14 18:22:55 +0300

    create lab3

---
+++ src/lab/useReliableFetch.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function useReliableFetch(url:string){
+    const[state, setState] = useState('')
+    return (
+        <div>
+            {/* data */}
+            {/* error */}
+            {/* isLoading */}
+        </div>
+    )
+}
