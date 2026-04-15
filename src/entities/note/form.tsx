76a451e2e07adb5ddc2ae04e1bd610d420ef41d2
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-15 17:42:04 +0300

    update note app

--- src/entities/note/form.tsx
+++ src/entities/note/form.tsx
@@ -1,39 +1,29 @@
-import { useState } from 'react';
-
-type Prop = {
-  onCreate: (title: string, text: string) => void;
+type PropForm = {
+    title: string,
+    text: string,
+    setTitle: (value: string) => void,
+    setText: (value: string) => void,
+    onSubmit: (e: React.SubmitEvent<HTMLFormElement>) => void,
 };
 
-
-export default function Form ( {onCreate}: Prop) {
-    const [title,setTitle] = useState('');
-    const [text, setText] = useState('');
-    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>){
-        e.preventDefault();
-
-        if (!title.trim() || !text.trim()) return;
-
-        onCreate(title,text);
-        setText('');
-        setTitle('');
-    }
+export default function Form ({title, text, setTitle, setText, onSubmit}: PropForm ) {
     return (
         <div >
             <form 
-                className="flex flex-col gap-[10px]"
-                onSubmit={handleSubmit}>
+                className="flex flex-col gap-[10px] w-[fit]"
+                id="note-form"
+                onSubmit={onSubmit}>
                 <input 
                     className="px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none focus:border-[#1976d3] font-[Roboto, sans-serif]"
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}>
                 </input>
                 <textarea
-                    className="px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none resize-none focus:border-[#1976d3] font-[Roboto, sans-serif]"
+                    rows={4}
+                    className="h-[] px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none resize-none focus:border-[#1976d3] font-[Roboto, sans-serif]"
                     value={text}
                     onChange={(e) => setText(e.target.value)}>
                 </textarea>
-                <button className='flex self-center uppercase px-6 py-2 text-white bg-[#1976d3] rounded-[5px] font-[Roboto, sans-serif] font-medium w-fit transition-all duration-200
-    hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer' type="submit">Save</button>
             </form>
         </div>
 
--- src/entities/note/note.tsx
+++ src/entities/note/note.tsx
@@ -4,11 +4,12 @@ import { useState } from 'react';
 
 const container = 'flex flex-col items-center';
 const noteContainer = 'flex flex-col  pt-[10px] pb-[30px] gap-[15px] w-fit';
-const headerNote = 'flex flex-row gap-[15px]';
+const headerNote = 'flex flex-col items-center gap-[15px] w-[350px]';
+const headerNoteBtn = 'flex flex-row gap-[10px]';
 const btnCreate = 'flex uppercase px-6 py-2 text-white bg-[#1976d3] rounded-[5px] font-[Roboto, sans-serif] font-medium transition-all duration-200 hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer';
 const btnClear = 'flex uppercase px-6 py-2 text-[#1976d3] bg-white border-[1px] border-[#1976d3] rounded-[5px] font-[Roboto, sans-serif] font-medium transition-all duration-200 hover:border-[#1976d3]/50 hover:text-[#1976d3]/50 cursor-pointer';
 const formContainer = 'flex flex-col gap-[10px] w-full';
-const formCross = 'flex justify-end text-[#1976d3] cursor-pointer';
+const btnClose = 'flex self-center uppercase px-6 py-2 text-white bg-[#1976d3] rounded-[5px] font-[Roboto, sans-serif] font-medium w-fit transition-all duration-200 hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer'
 const arrContainer = 'flex flex-col gap-[20px] px-[20px] w-full';
 const itemNote = 'border-y-[1px] border-[#1976d3]/50';
 const itemNoteTitle = 'text-[#1976d3] text-[32px] font-[Roboto, sans-serif]';
@@ -19,6 +20,8 @@ const arrEmpty = 'flex flex-col items-center opacity-50';
 export default function Note(){
   const [isOpen, setIsOpen] = useState(false)
   const [note, setNote] = useState<Note[]>([]);
+  const [title,setTitle] = useState('');
+  const [text, setText] = useState('');
 
   function onCreate(title: string, content: string) {
     const newNote: Note = {
@@ -30,27 +33,46 @@ export default function Note(){
     setNote((prev) => [newNote, ...prev]);
     setIsOpen(false);
   }
-  function onClean(){
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>){
+      e.preventDefault();
+
+      if (!title.trim() || !text.trim()) return;
+
+      onCreate(title,text);
+      setText('');
+      setTitle('');
+  }
+
+  function handleCleanNotes(){
     setNote([])
+  }
+
+  function handleClearForm(){
+    setText('');
+    setTitle('');
   }
   return (
     <div className={container}>
       <div className={noteContainer}>
-        <div className={headerNote}>
-          <button className={btnCreate} onClick={() => setIsOpen(true)}>Create</button>
-          <button className={btnClear} onClick={onClean}>Clear</button>
-        </div>
         {isOpen ?
-        <div className={formContainer}>
-          <button className={formCross} onClick={() => setIsOpen(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          </button>
-          <Form onCreate={onCreate} /> 
-        </div>
-          
-          : null
+          <div className={headerNote}>
+            <div className={headerNoteBtn}>
+              <button className={btnCreate} type='submit' form="note-form">Save</button>
+              <button className={btnClear} onClick={handleClearForm}>Clear</button>
+            </div>
+            <div className={formContainer}>
+              <Form title={title} text={text} setTitle={setTitle} setText={setText} onSubmit={handleSubmit} /> 
+              <button className={btnClose} onClick={() => setIsOpen(false)}>Close</button>
+            </div>
+          </div>
+           : 
+          <div className={headerNote}>
+            <div className={headerNoteBtn}>
+              <button className={btnCreate} onClick={() => setIsOpen(true)}>Create</button>
+              <button className={btnClear} onClick={handleCleanNotes}>Clear</button>
+            </div>
+          </div>
         }
       </div>
       <div className={arrContainer}>
