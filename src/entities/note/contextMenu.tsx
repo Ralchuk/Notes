c169c3c59f64d6c7ee4e84c2542f11be22351baf
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-17 19:05:55 +0300

    work on Context Menu

--- src/entities/note/contextMenu.tsx
+++ src/entities/note/contextMenu.tsx
@@ -6,10 +6,11 @@ type PropContextMenu = {
 export default function ContextMenu({x, y}: PropContextMenu){
     return (
         <div 
-            className="flex fixed z-50" 
+            className="flex flex-col items-stretch fixed z-50 " 
             style={{left: x, top:y}}
             onClick={(e) => e.stopPropagation()}>
-            <p>MyContextMenu</p>
+            <button className="flex uppercase px-6 py-2 text-white bg-[#1976d3] font-[Roboto, sans-serif] text-xs font-medium transition-all duration-200 hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer">Edit note</button>
+            <button className="flex uppercase px-6 py-2 text-white bg-[#1976d3] font-[Roboto, sans-serif] text-xs font-medium transition-all duration-200 hover:bg-white hover: border-[#1976d3] hover: border-[1px] hover:text-[#1976d3] cursor-pointer">Delete note</button>
         </div>
     )
 }
