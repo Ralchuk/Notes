4e5be55fe514193b2f8782b09618256f7f29a79c
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-17 18:41:33 +0300

    work on Context Menu

--- src/entities/note/contextMenu.tsx
+++ src/entities/note/contextMenu.tsx
@@ -7,7 +7,8 @@ export default function ContextMenu({x, y}: PropContextMenu){
     return (
         <div 
             className="flex fixed z-50" 
-            style={{left: x, top:y}}>
+            style={{left: x, top:y}}
+            onClick={(e) => e.stopPropagation()}>
             <p>MyContextMenu</p>
         </div>
     )
