15ef9a986a5abcb66802c9af80a02fb8ab600f6e
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-24 16:19:01 +0300

    create  video lab DZ5

--- src/lab/Videopleyer.tsx
+++ src/lab/Videopleyer.tsx
@@ -1,8 +1,16 @@
+import videoSrc from "../../img/12099242_1920_1080_30fps.mp4";
 
-export default function Videopleyer (){
-    return (
-        <div>
-            <video src="../../img/12099242_1920_1080_30fps.mp4" autoPlay muted loop controls></video>
-        </div>
-    )
+export default function Videopleyer() {
+  const videoRef = (node: HTMLVideoElement | null) => {
+    if (node) {
+      console.log("mounted");
+    } else {
+      console.log("unmounted");
+    }
+  };
+  return (
+    <div>
+      <video ref={videoRef} src={videoSrc} autoPlay muted loop></video>
+    </div>
+  );
 }
