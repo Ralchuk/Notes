820bccbf4509fb8633f2907b666f09635983fc43
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-24 11:27:02 +0300

    set on component Notes useStickyState

--- src/entities/note/form.tsx
+++ src/entities/note/form.tsx
@@ -1,8 +1,8 @@
 import { type PropForm } from './model/types';
 
 const formWrapper = 'flex flex-col gap-[10px] w-[fit]';
-const formTitle = 'px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none focus:border-[#1976d3] font-[Roboto, sans-serif]';
-const formText = 'h-[] px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none resize-none focus:border-[#1976d3] font-[Roboto, sans-serif]';
+const formTitle = 'px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none focus:border-[#1976d3] font-[Roboto, sans-serif] placeholder:italic focus:placeholder-transparent';
+const formText = 'px-3 py-1 rounded-[5px] border-[1px] border-[#1976d3]/40 outline-none resize-none focus:border-[#1976d3] font-[Roboto, sans-serif] placeholder:italic focus:placeholder-transparent';
 
 export default function Form({title, text, setTitle, setText, onSubmit}: PropForm ) {
   return (
