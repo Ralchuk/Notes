3e5eb0f6fa75efc9a058708b9d2cae40bdb13ae2
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-29 15:35:22 +0300

    done  lab 6...

--- src/lab/TimerLab.tsx
+++ src/lab/TimerLab.tsx
@@ -1,51 +1,122 @@
-import {useState, useReducer, useEffect} from "react";
+import {useReducer, useEffect} from "react";
+
+interface TimerState {
+    time: number,
+    isRunning: boolean,
+    lap: number[],
+};
+
+type TimerAction = 
+    | {
+        type: 'START';
+    }
+    | {
+        type: 'LAP';
+        payload: {time: number};
+    }
+    | {
+        type: 'PAUSE';
+    }
+    | {
+        type: 'RESET';
+    }
+    | {
+        type: 'TICK';
+    };
 
 
-/* state for timer 
- для кругов использовать круги
- пауза
- ресет */
 
+const initialState: TimerState = {
+    time: 0,
+    isRunning: false,
+    lap: []
+};
 
+function reducer(state: TimerState, action: TimerAction): TimerState {
+    switch (action.type) {
+        case 'START': 
+            return {
+                ...state,
+                isRunning: true,
+            };
+        case 'LAP':
+            return {
+                ...state,
+                lap:[...state.lap, action.payload.time],
+            };
+        case 'PAUSE':
+            return {
+                ...state,
+                isRunning: false,
+            };
+        case 'RESET':
+            return {
+                ...state,
+                isRunning:false,
+                time: 0,
+                lap:[],
+            }
+        case 'TICK':
+            return {
+                ...state,
+                time: state.time + 1,
+            }; 
+    
+        default:
+            return state;
+    }
+}
 
 const TimerLab = () => {
-    const [time, setTime] = useState<number>(0)
-    const [isRunning, setIsRunning] = useState(false)
-    const [lap, setLap] = useState<number[]>([])
+
+    const[state, dispatch] = useReducer(reducer, initialState);
+
+    // const [time, setTime] = useState<number>(0);
+    // const [isRunning, setIsRunning] = useState(false);
+    // const [lap, setLap] = useState<number[]>([]);
 
     function handleClick(){
-        setIsRunning(true);
+        dispatch({type: 'START'})
+        // setIsRunning(true);
     }
 
     function handlePause(){
-        setIsRunning(false)
+        dispatch({type: 'PAUSE'})
+        // setIsRunning(false);
     }
 
     function handlLap(){
-        setLap([...lap, time])
+        dispatch({type: 'LAP',
+            payload: {time: state.time},
+        });
+        // setLap([...lap, time]);
     }
     function handleReset(){
-        setIsRunning(false)
-        setTime(0)
+        dispatch({type: 'RESET'})
+        // setIsRunning(false);
+        // setTime(0);
     }
 
 
     useEffect(() => {
-        if (isRunning === false) return; 
-        const id = setInterval(() => setTime((prev) => prev + 1), 1000);
+        if (state.isRunning === false) return; 
+        const id = setInterval(() => dispatch({type: 'TICK'}), 1000);
         return () => clearInterval(id);
         
-    },[isRunning]);
+    },[state.isRunning]);
 
     return (
         <div className="flex flex-col  items-center">
-            <p>{time}</p>
-                {lap.map((item, index) => (
-                    <div key={index}>
-                        {item}
+            <p>{state.time}</p>
+            <div>
+                {state.lap.map((item, index) => (
+                    <div className="flex text-start" key={index}>
+                        Lap {index +1 } = {item} sec
                     </div>
                 )
-            )}
+                )}
+            </div>
+            
             <div>
             <button className="py-1 px-4 rounded-[5px] border-[1px]" onClick={handleClick}>GO</button>
             <button className="py-1 px-4 rounded-[5px] border-[1px]" onClick={handlLap}>Set Lap</button>
