8cd67ddfbb7c57dcbf0981528b452732d984c766
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-05-08 12:34:09 +0300

    create TooltipLab dz8

--- src/lab/Accordion.tsx
+++ src/lab/Accordion.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, type ReactNode } from "react";
+import { createContext, useContext, type ReactNode } from 'react';
 import { useState } from 'react';
 
 interface AccordionContextType {
